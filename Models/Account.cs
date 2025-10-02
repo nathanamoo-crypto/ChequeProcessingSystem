eccd06857ea8f8571d4de354a2987f650c522c9d
kbweeb <172048558+kbweeb@users.noreply.github.com>
2025-10-02 11:04:34 +0000

    so yeah did it

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,20 +1,17 @@
 ﻿using ChequeProcessingSystem.Models;
 
-namespace ChequeProcessingSystem.Models
+public class Account
 {
-    public class Account
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string AccountNumber { get; set; } = string.Empty;
+    public string AccountNumber { get; set; } = string.Empty;
 
-        public string AccountName { get; set; } = string.Empty;
+    public string HolderName { get; set; } = string.Empty; // <-- RENAME from AccountName
 
-        public decimal Balance { get; set; }
+    public decimal Balance { get; set; }
 
-        public string AccountType { get; set; } = string.Empty;
+    public string AccountType { get; set; } = string.Empty;
 
-        // Navigation: An Account can have many Cheques
-        public ICollection<Cheque> Cheques { get; set; } = new List<Cheque>();
-    }
+    // Navigation: An Account can have many Cheques
+    public ICollection<Cheque> Cheques { get; set; } = new List<Cheque>();
 }
