ab61046164c601c8f1186df2e711dfee946bcac2
Nathaniel <amoonathan111@gmail.com>
2025-10-02 12:25:59 +0000

    Controller fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,36 @@
-using System.Diagnostics;
 using ChequeProcessingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace ChequeProcessingSystem.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ChequeContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ChequeContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Load cheques including Account (so HolderName isn’t null)
+            var cheques = _context.Cheques
+                                  .Include(c => c.Account)
+                                  .ToList();
+
+            return View(cheques);
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
